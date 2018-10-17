18cefc9facdc3f82b8551ba96798dd14c4bd04cb
unknown <businlee@gmail.com>
2018-10-18 00:34:39 +0700

    Menu done

--- TradingBook/app/src/main/java/com/example/anhki/tradingbook/View/HomeActivity.java
+++ TradingBook/app/src/main/java/com/example/anhki/tradingbook/View/HomeActivity.java
@@ -2,23 +2,30 @@ package com.example.anhki.tradingbook.View;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.anhki.tradingbook.R;
 
 public class HomeActivity extends AppCompatActivity {
+    Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menutrangchu,menu);
+        return true;
     }
 
     @Override
