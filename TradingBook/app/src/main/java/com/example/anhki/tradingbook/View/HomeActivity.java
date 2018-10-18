62feb97ebeaa9d9c0d46e1a77db2de573132db0e
unknown <businlee@gmail.com>
2018-10-19 01:15:25 +0700

    Coding TabLayout

--- TradingBook/app/src/main/java/com/example/anhki/tradingbook/View/HomeActivity.java
+++ TradingBook/app/src/main/java/com/example/anhki/tradingbook/View/HomeActivity.java
@@ -3,23 +3,44 @@ package com.example.anhki.tradingbook.View;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.anhki.tradingbook.R;
+import com.example.anhki.tradingbook.View.Adapter.ViewPagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
     Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            if (i == 0)
+                tabLayout.getTabAt(i).setIcon(R.drawable.ic_baseline_home_black_18dp);
+            if (i == 1)
+                tabLayout.getTabAt(i).setIcon(R.drawable.ic_baseline_face_black_18dp);
+            if (i == 2)
+                tabLayout.getTabAt(i).setIcon(R.drawable.ic_outline_more_horiz_black_18dp);
+        }
     }
 
     @Override
---
+++ TradingBook/app/src/main/java/com/example/anhki/tradingbook/View/View/TrangChu/Fragment/FragmentUser.java
@@ -0,0 +1,20 @@
+package com.example.anhki.tradingbook.View.View.TrangChu.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.anhki.tradingbook.R;
+
+public class FragmentUser extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_home_user, container, false);
+        return view;
+    }
+}
