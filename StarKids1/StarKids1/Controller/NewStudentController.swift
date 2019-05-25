a9c1cef0b3dd0eb22ddf881dfac113b67f7f6315
Thanh Lê <thanhle@Thanhs-Mac.local>
2019-05-25 00:32:21 -0700

    HCI IU

--- StarKids1/StarKids1/Controller/NewStudentController.swift
+++ StarKids1/StarKids1/Controller/NewStudentController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
-import Photos
+import Firebase
+
+
+let storage = Storage.storage()
+let storageRef = storage.reference(forURL: "gs://starkids1-fda2a.appspot.com")
 
 class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var lbValid: UILabel!
     @IBOutlet weak var txtBirthYear: UITextField!
     @IBOutlet weak var txtFullName: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var btnAddStudent: UIButton!
     @IBOutlet weak var imgAvatar: UIImageView!
     @IBOutlet weak var pickerBirthDay: UIPickerView!
@@ -28,7 +33,7 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
         super.viewDidLoad()
         pickerBirthDay.delegate = self
         pickerBirthDay.dataSource = self
-        
+        imgData = UIImage(named: "camera")!.pngData()
         for i in 0...1 {
             var row = [Int]()
             for j in 0...30 {
@@ -53,13 +58,16 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
         let btnPhoto:UIAlertAction = UIAlertAction(title: "Photo", style: .default) { (UIAlertAction) in
             let imgPicker = UIImagePickerController()
             imgPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-            //imgPicker.delegate = self
-            imgPicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imgPicker.delegate = self
             imgPicker.allowsEditing = false
             self.present(imgPicker, animated: true, completion: nil)
         }
         let btnCamera:UIAlertAction = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
-            
+            let imgPicker = UIImagePickerController()
+            imgPicker.sourceType = UIImagePickerController.SourceType.camera
+            imgPicker.delegate = self
+            imgPicker.allowsEditing = false
+            self.present(imgPicker, animated: true, completion: nil)
         }
         alert.addAction(btnPhoto)
         alert.addAction(btnCamera)
@@ -67,6 +75,15 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
         self.present(alert, animated: true, completion: nil)
     }
     @IBAction func btn_AddStudent(_ sender: Any) {
+        
+        let alertActivity:UIAlertController = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        let activity:UIActivityIndicatorView = UIActivityIndicatorView(style: .whiteLarge)
+        activity.frame = CGRect(x: view.frame.size.width/2-20, y: 25, width: 0, height: 0)
+        activity.color = UIColor.init(displayP3Red: CGFloat(254)/255, green: CGFloat(229)/255, blue: CGFloat(139)/255, alpha: 1.0)
+        alertActivity.view.addSubview(activity)
+        activity.startAnimating()
+        self.present(alertActivity, animated: true, completion: nil)
+        
         if ((month == "04" || month == "06" || month == "09" || month == "11") && day == "31") {
             lbValid.isHidden = false
         } else {
@@ -79,6 +96,47 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
                     date = day + month + txtBirthYear.text!
                     lbValid.isHidden = true
                 }
+            }
+        }
+        let email:String = txtEmail.text!
+        let password:String = date
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if (error == nil)
+            {
+                Auth.auth().signIn(withEmail: email, password: password) { user, error in
+                    if (error == nil)
+                    {
+                        print("Đăng nhập thành công")
+                    }
+                }
+                
+                let avatarRef = storageRef.child("avatars/\(email).jpg")
+                let uploadTask = avatarRef.putData(self.imgData, metadata: nil) { metadata, error in
+                    guard let metadata = metadata else {
+                        print("Lỗi up avatar")
+                        return
+                    }
+                    let size = metadata.size
+                    avatarRef.downloadURL { (url, error) in
+                        
+                        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                        changeRequest?.displayName = self.txtFullName.text!
+                        changeRequest?.photoURL = url
+                        changeRequest?.commitChanges { (error) in
+                            if (error == nil){
+                                activity.stopAnimating()
+                                self.gotoScreen()
+                            } else {
+                                print("Lỗi update profile")
+                            }
+                        }
+                    }
+                }
+                uploadTask.resume()
+            }
+            else
+            {
+                print("Lỗi đăng ký!")
             }
         }
     }
@@ -94,7 +152,7 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return String(arrDate[component][row])
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
         if (component == 0) {
@@ -115,15 +173,6 @@ class NewStudentController: UIViewController, UIPickerViewDelegate, UIPickerView
 }
 extension NewStudentController : UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-//    let photos = PHPhotoLibrary.authorizationStatus()
-//    if (photos == .notDetermined) {
-//    PHPhotoLibrary.requestAuthorization({status in
-//    if status == .authorized{
-//    ...
-//    } else {}
-//    })
-//    }
-    
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
